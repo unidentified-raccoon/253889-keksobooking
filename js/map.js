ced999eaa39d60c936c3634141c41cfe4f9d732f
Yana Denisenko <yanadmail@gmail.com>
2018-06-20 14:34:04 -0700

    Drag-and-drop on main pin added

--- js/map.js
+++ js/map.js
@@ -307,3 +307,65 @@ map.addEventListener('keydown', function (evt) {
   numberOfGuestsField.addEventListener('change', roomsGuestsValidation);
 
 })();
+
+// ------------------------------------------
+// moving the pin var pinMain
+
+var PIN_LOCATION_Y_MIN = 130; // px
+var PIN_LOCATION_Y_MAX = 630; // px
+var PIN_LOCATION_X_MIN = 0;
+var PIN_LOCATION_X_MAX = 1135; // px
+
+pinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  function onMouseMove(moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var pinLocationTopEdge = PIN_LOCATION_Y_MIN - MAIN_PIN_HEIGHT;
+    var pinLocationBottomEdge = PIN_LOCATION_Y_MAX - MAIN_PIN_HEIGHT;
+    var pinLocationLeftEdge = PIN_LOCATION_X_MIN;
+    var pinLocationRightEdge = PIN_LOCATION_X_MAX;
+
+    var top = pinMain.offsetTop - shift.y;
+    var left = pinMain.offsetLeft - shift.x;
+
+    if (top <= pinLocationTopEdge) {
+      pinMain.style.top = pinLocationTopEdge + 'px';
+    } else if (top >= pinLocationBottomEdge) {
+      pinMain.style.top = pinLocationBottomEdge + 'px';
+    } else if (left <= pinLocationLeftEdge) {
+      pinMain.style.left = pinLocationLeftEdge + 'px';
+    } else if (left >= pinLocationRightEdge) {
+      pinMain.style.left = pinLocationRightEdge + 'px';
+    } else {
+      pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+      pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+    }
+
+  }
+
+  function onMouseUp(upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
